3f463660ceafe3301b841c493bcd63276fd490b0
Kate Caldwell <kate.kec.caldwell@gmail.com>
2017-05-08 19:12:44 -0400

    feat(project dropdown): selecting project in toolbar dispatches it to store

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -4,6 +4,7 @@ const rootReducer = combineReducers({
   auth: require('./auth').default,
   blurbs: require('./blurbs').default,
   containers: require('./containers').default,
+  projects: require('./projects').default
 })
 
 export default rootReducer
