63ad5634c2b5f7f8f88d6eafdf9023208974c3a9
Jodie Ly <jodie.ly@gmail.com>
2017-05-08 15:24:45 -0400

    added reducers and express routes for blurbs and containers

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
   auth: require('./auth').default,
+  blurbs: require('./blurbs').default,
+  containers: require('./containers').default,
 })
 
 export default rootReducer
