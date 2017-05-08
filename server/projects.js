1ff63183317c5c5056dc52db0d43ede039915ac8
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-05-08 16:21:19 -0400

    feat(routes): GET projectid including container and blurb  with associated tests

--- server/projects.js
+++ server/projects.js
@@ -2,10 +2,32 @@
 
 const db = require('APP/db')
 const Project = db.model('projects')
+const Container = db.model('containers')
+const Blurb = db.model('blurbs')
+
 
 module.exports = require('express').Router()
+  // fetches all projects for drop down
   .get('/', (req, res, next) =>
     Project.findAll()
-    .then(projects => res.send(projects))
+    .then(projects => {
+      res.send(projects)
+    })
+    .catch(next)
+  )
+  // fetches all containers/blurbs for the selected project
+  .get('/:projectId', (req, res, next) =>
+    Project.findOne({
+      where: {
+        id: req.params.projectId
+      },
+      include: [{
+        model: Container,
+        include: [{
+          model: Blurb
+        }]
+      }]
+    })
+    .then(project => res.send(project))
     .catch(next)
   )
--- server/projects.test.js
+++ server/projects.test.js
@@ -1,19 +1,70 @@
 const request = require('supertest')
-    , { expect, describe } = require('chai')
-    , db = require('APP/db')
-    , app = require('./start')
+  , { expect } = require('chai')
+  , Promise = require('bluebird')
+  , db = require('APP/db')
+  , app = require('./start')
 
-/* global describe it before afterEach */
+const Project = db.model('projects')
+const Container = db.model('containers')
+const Blurb = db.model('blurbs')
+
+/* global describe it before afterEach, beforeEach */
 
 describe('/api/projects', () => {
+  var createdEntries = {}
   before('Await database sync', () => db.didSync)
+  beforeEach('Add projects to db', () => {
+    return Promise.all(
+      [
+        Project.bulkCreate([
+        { title: 'ProjectOne' },
+        { title: 'ProjectTwo' }
+        ], {returning: true}),
+        Container.bulkCreate([
+        { title: 'ContainerOne' },
+        { title: 'ContainerTwo' }
+        ], {returning: true}),
+        Blurb.bulkCreate([
+        { title: 'BlurbOne' },
+        { title: 'BlurbTwo' }
+        ], {returning: true})
+      ]
+    ).spread((projects, containers, blurbs) => {
+      blurbs.forEach((blurb, indx) => {
+        blurb.setContainer(containers[indx])
+        createdEntries[blurb.title] = blurb
+      })
+      containers.forEach((container, indx) => {
+        container.setProject(projects[indx])
+        createdEntries[container.title] = container
+      })
+      projects.forEach((project) => {
+        createdEntries[project.title] = project
+      })
+    })
+  })
+
   afterEach('Clear the tables', () => db.truncate({ cascade: true }))
 
   describe('GET /', () =>
     it('returns all project titles', () =>
       request(app)
-      .get('/api/projects')
-      .then(res => expect(res.body).to.have.lengthOf(3))
+        .get('/api/projects')
+        .then(res => {
+          expect(res.body).to.have.lengthOf(2)
+        })
     )
   )
+
+  describe('GET /:projectId', () => {
+    it('returns the project', () =>
+      request(app)
+        .get('/api/projects/' + createdEntries.ProjectOne.id)
+        .then(res => {
+          expect(res.body.title).to.equal('ProjectOne')
+          expect(res.body.containers[0].title).to.equal('ContainerOne')
+          expect(res.body.containers[0].blurbs[0].title).to.equal('BlurbOne')
+        })
+    )
+  })
 })
