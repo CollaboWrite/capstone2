08162324c7410b2eb048b71da23c96cf2e46f6b7
Sarah Herr <sarah.herr@hotmail.com>
2017-05-08 12:53:34 -0400

    wireframe view

---
+++ app/components/Trashcan.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default (props) =>
+  <div>
+    <h3>Trashcan</h3>
+    <ul>
+      <li>File 1</li>
+      <li>File 2</li>
+      <li>File 3</li>
+      <li>File 4</li>
+    </ul>
+  </div>
